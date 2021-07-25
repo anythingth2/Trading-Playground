f94d8bc074e796bf3f7c19debd0cdd398e8053a6
ChiChaChai <chatchaishaetan@gmail.com>
2021-07-25 21:23:07 +0700

    Add grid adative zone

--- strategy/GridAdaptiveZoneStrategy.py
+++ strategy/GridAdaptiveZoneStrategy.py
@@ -3,7 +3,7 @@ import backtrader as bt
 from indicator import LinearIndicator, HorizontalLinearIndicator
 import numpy as np
 import pandas as pd
-
+import math
 
 class GridBar:
     def __init__(self,
@@ -22,13 +22,14 @@ class GridBar:
         self.grid_crossover = grid_crossover
 
 
-class GridBasicStrategy(bt.Strategy):
+class GridAdaptiveZoneStrategy(bt.Strategy):
 
     params = {
         'n_grid': 32,
         'zone': {
-            'top_grid_price': 10000,
-            'bottom_grid_price': 2200
+            'start_price': 8000,
+            'high_side_ratio': 0.3,
+            'low_side_ratio': 0.7
         },
         'position': {
             'type': 'FIX_CASH',
@@ -51,13 +52,18 @@ class GridBasicStrategy(bt.Strategy):
         #                              index=pd.DatetimeIndex(data=[], name='time'))
 
     def __create_grid_bars(self):
-        grid_size = (self.p.zone['top_grid_price'] -
-                     self.p.zone['bottom_grid_price']) / (self.p.n_grid+1)
+        self.base_grid_price = self.p.zone['start_price']
+        self.top_grid_price = self.base_grid_price * (1 + self.p.zone['high_side_ratio'])
+        self.bottom_grid_price = self.base_grid_price * (1 - self.p.zone['low_side_ratio'])
+        
+
+        grid_size = (self.top_grid_price -
+                     self.bottom_grid_price) / (self.p.n_grid+1)
         self.grids = []
         for grid_no in range(self.p.n_grid):
-            price = self.p.zone['bottom_grid_price'] + grid_size*grid_no
+            price = self.bottom_grid_price + grid_size*grid_no
             # Take profit at next grid bar
-            tp_price = self.p.zone['bottom_grid_price'] + grid_size*(grid_no+1)
+            tp_price = self.bottom_grid_price + grid_size*(grid_no+1)
             grid_line = HorizontalLinearIndicator(
                 y=price, plot=self.p.plot['plot_grid_bar'], plotname=f'bar_{grid_no}')
             grid_crossover = bt.indicators.CrossOver(
@@ -73,10 +79,39 @@ class GridBasicStrategy(bt.Strategy):
             )
             self.grids.append(grid)
 
+    def create_new_zone(self, new_base_grid_price):
+        self.base_grid_price = new_base_grid_price
+        self.top_grid_price = self.base_grid_price * (1 + self.p.zone['high_side_ratio'])
+        self.bottom_grid_price = self.base_grid_price * (1 - self.p.zone['low_side_ratio'])
+        
+        grid_size = (self.top_grid_price -
+                     self.bottom_grid_price) / (self.p.n_grid+1)
+        
+        for grid in self.grids:
+            grid_no = grid.grid_no
+            price = self.bottom_grid_price + grid_size*grid_no
+            # Take profit at next grid bar
+            tp_price = self.bottom_grid_price + grid_size*(grid_no+1)
+
+            grid.grid_price = price
+            grid.grid_indicator.update_y(price)
+            grid.grid_tp = tp_price
+        
+        
+    
     def next(self):
+
+        # if math.isclose(self.data.datetime[0], 737524):
+        #     for grid in self.grids:
+        #         grid.grid_indicator.update_range(end_datetime=self.data.datetime[0])
         for grid in self.grids:
             if grid.grid_crossover[0] != 0:
                 self.notify_grid(grid)
+
+        # Create new zone if price goes over our current zone
+        if self.data.close[0] < self.bottom_grid_price or self.data.close[0] > self.top_grid_price:
+            self.create_new_zone(self.data.close[0])
+            
 
     def notify_grid(self, grid):
 
@@ -93,19 +128,21 @@ class GridBasicStrategy(bt.Strategy):
             f'open buy #{buy_order.ref} {grid.grid_price:.3f}, tp #{tp_order.ref}: {grid.grid_tp:.3f}, sl #{sl_order.ref}: {grid.grid_sl}')
 
     def notify_order(self, order):
-        if order.status in [order.Completed]:
-            if order.isbuy():
-                self.log(
-                    f'BUY EXECUTED, #{order.ref} {order.executed.price:.3f}')
+        # if order.status in [order.Completed]:
+        #     if order.isbuy():
+        #         self.log(
+        #             f'BUY EXECUTED, #{order.ref} {order.executed.price:.3f}')
 
-                # buy_order_df = pd.DataFrame([{
-                #     'price': order.executed.price,
-                #     'share': self.p.grid_share
-                # }], index=pd.DatetimeIndex(data=[bt.num2date(order.executed.dt)], name='time'))
-                # self.order_df = pd.concat([self.order_df, buy_order_df])
-                # print(self.position)
-            elif order.issell():
-                self.log(
-                    f'SELL EXECUTED, #{order.ref} {order.executed.price:.3f}')
-                # print(self.position)
+        #         # buy_order_df = pd.DataFrame([{
+        #         #     'price': order.executed.price,
+        #         #     'share': self.p.grid_share
+        #         # }], index=pd.DatetimeIndex(data=[bt.num2date(order.executed.dt)], name='time'))
+        #         # self.order_df = pd.concat([self.order_df, buy_order_df])
+        #         # print(self.position)
+        #     elif order.issell():
+        #         self.log(
+        #             f'SELL EXECUTED, #{order.ref} {order.executed.price:.3f}')
+        #         # print(self.position)
+        pass
+    def notify_trade(self, trade):
         pass
